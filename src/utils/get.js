18cf876a66255dc0df08886e761211901f05fa95
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2021-08-24 12:56:33 +0300

    Add get helper

---
+++ src/utils/get.js
@@ -0,0 +1,11 @@
+export default function get(obj, path = '', defaultValue) {
+    const parts = path.split('.');
+    let res = obj;
+    for (let i = 0; i < parts.length; i++) {
+        res = res[parts[i]]
+        if (res === undefined) {
+            return defaultValue;
+        }
+    }
+    return res ?? defaultValue;
+};
