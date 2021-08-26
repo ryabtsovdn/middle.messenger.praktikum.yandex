a3b1d99a705973bc2131ea1c8ef82f323c0033cc
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2021-08-27 00:45:22 +0300

    Update routes and add correct context

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
+import avatar from 'url:../static/img/default-user.svg';
+
 const chat = import('./components/pages/chat-page');
 const login = import('./components/pages/login-page');
 const register = import('./components/pages/register-page');
-const internalError = import('./components/pages/500');
-const notFound = import('./components/pages/404');
+const profile = import('./components/pages/profile-page');
+const error = import('./components/pages/error-page');
 
 const root = document.getElementById('root');
 
+const user = {
+    email: 'email@yandex.ru',
+    login: 'ivanivanov',
+    firstName: 'Иван',
+    secondName: 'Иванов',
+    displayName: 'Иван',
+    phone: '+7 (999) 999 99 99',
+    avatar,
+};
+
 const routes = {
-    '/': chat,
-    '/login': login,
-    '/register': register,
-    '/500': internalError,
-    '/404': notFound,
+    '/': [chat, {}],
+    '/login': [login, {}],
+    '/register': [register, {}],
+    '/profile': [profile, {page: {state: 'profile'}, form: {hideSubmit: true}, user}],
+    '/profile/edit': [profile, {page: {state: 'edit'}, user}],
+    '/profile/password': [profile, {page: {state: 'password'}, user}],
+    '/profile/avatar': [profile, {page: {state: 'avatar'}, user}],
+    '/500': [error, {code: 500, message: 'Мы уже фиксим'}],
+    '/404': [error, {code: 404, message: 'Не туда попали'}],
 };
 
 const navigate = async () => {
-    const page = routes[location.pathname] || notFound;
+    const [page, ctx] = routes[location.pathname] || routes['/404'];
     page.then(tmpl => {
-        root.replaceChildren(tmpl.render({}));
+        root.replaceChildren(tmpl.render(ctx));
     });
 };
 
