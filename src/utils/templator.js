f6f42ba53278487bc7d45ade71f0bd505cf9f011
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2021-08-26 22:54:41 +0300

    Templator: support negation in #if helper

--- src/utils/templator.js
+++ src/utils/templator.js
@@ -41,11 +41,6 @@ export class Templator {
         return Templator.partials[key].compile(partialCtx);
     }
 
-    _getReplaceRegex(str, flags = 'gi') {
-        str = str.replace('?', '.')
-        return new RegExp(str, flags);
-    }
-
     _compileTemplate(ctx) {
         let match;
         let tmpl = this._template;
@@ -55,33 +50,39 @@ export class Templator {
             }
             if (match[1].startsWith('#if')) {
                 const paramString = match[1].slice(3).trim();
-                const [key, value = true] = paramString.split(' ');
+                let [key, value = true] = paramString.split(' ');
+                let isNegation = false;
+                if (key.startsWith('!')) {
+                    isNegation = true;
+                    key = key.slice(1);
+                }
                 const data = get(ctx, key, '');
                 const re = new RegExp(`${match[0]}((?!\/if).*?){{\/if}}`, 'msi');
-                tmpl = tmpl.replace(re, data === value ? '$1' : '');
+                const isVisible = isNegation && data !== value || !isNegation && data === value;
+                tmpl = tmpl.replace(re, isVisible ? '$1' : '');
                 this.TMPL_RE.lastIndex = match.index;
                 continue;
             }
             if (match[1].startsWith('#>')) {
                 const partial = match[1].slice(2).trim();
                 const partialCompiled = this._compilePartial(partial, ctx, true);
-                tmpl = tmpl.replace(this._getReplaceRegex(match[0]), partialCompiled);
+                tmpl = tmpl.replace(new RegExp(match[0], 'gi'), partialCompiled);
                 continue;
             }
             if (match[1].startsWith('>')) {
                 const partial = match[1].slice(1).trim();
                 const partialCompiled = this._compilePartial(partial, ctx);
-                tmpl = tmpl.replace(this._getReplaceRegex(match[0]), partialCompiled);
+                tmpl = tmpl.replace(new RegExp(match[0].replace('?', '.'), 'gi'), partialCompiled);
                 continue;
             }
             const prop = match[1].trim();
             const data = get(ctx, prop, '');
             if (typeof data === 'function') {
                 window[prop] = data;
-                tmpl = tmpl.replace(this._getReplaceRegex(match[0]), `window.${prop}()`);
+                tmpl = tmpl.replace(new RegExp(match[0], 'gi'), `window.${prop}()`);
                 continue;
             }
-            tmpl = tmpl.replace(this._getReplaceRegex(match[0]), data);
+            tmpl = tmpl.replace(new RegExp(match[0], 'gi'), data);
         }
         return tmpl;
     }
