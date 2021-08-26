4a67110821a18f210575ba44ace45a00458a401a
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2021-08-27 00:36:35 +0300

    Add inline version of form field molecule

--- src/components/molecules/form-field/form-field.tmpl.js
+++ src/components/molecules/form-field/form-field.tmpl.js
@@ -1,5 +1,5 @@
 export default `
-    <div class="form-field">
+    <div class="form-field {{className}}">
         {{> atoms-input className=form-field__input}}
         <label class="form-field__label" for={{name}}>
             {{label}}
