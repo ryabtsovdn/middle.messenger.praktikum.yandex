a7d84cc6f0aa57f4c36814bca391028c1a0db213
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2021-08-27 00:42:42 +0300

    Add profile page

---
+++ src/components/pages/profile-page/index.js
@@ -0,0 +1,2 @@
+export * from './profile-page';
+export {template} from './profile-page.tmpl';
---
+++ src/components/pages/profile-page/profile-page.tmpl.js
@@ -0,0 +1,26 @@
+export default `
+    <div class="profile-page">
+        <aside class="profile-page__aside">
+            {{> atoms-link href="/" className="profile-page__back" text=""}}
+        </aside>
+        <main class="profile-page__content">
+            {{> organisms-avatar className=profile-page__avatar}}
+            {{#if !page.state password}}
+                {{> organisms-profile-form}}
+            {{/if}}
+            {{#if page.state password}}
+                {{> organisms-password-form}}
+            {{/if}}
+            {{#if page.state profile}}
+                <div class="profile-page__links">
+                    {{> atoms-link href="/profile/edit" className="profile-page__link" text="Изменить данные"}}
+                    {{> atoms-link href="/profile/password" className="profile-page__link" text="Изменить пароль"}}
+                    {{> atoms-link href="/" className="profile-page__link link_danger" text="Выйти"}}
+                </div>
+            {{/if}}
+            {{#if page.state avatar}}
+                {{#> templates-modal content=organisms-avatar-form className=profile-page__avatar-modal}}
+            {{/if}}
+        </main>
+    </div>
+`;
