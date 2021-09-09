66ab912f4f9665512ef2f9230463914d68375d6f
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2021-09-10 00:41:39 +0300

    Fix css errors and warning

--- src/components/pages/login-page/login-page.ts
+++ src/components/pages/login-page/login-page.ts
@@ -2,7 +2,6 @@ import {Templator} from '../../../utils/templator';
 import template from './login-page.tmpl';
 import '../../organisms/signin-form';
 import '../../templates/auth-template';
-import './login-page.css';
 
 const tmpl = new Templator(template);
 
--- src/components/pages/register-page/register-page.ts
+++ src/components/pages/register-page/register-page.ts
@@ -2,7 +2,6 @@ import {Templator} from '../../../utils/templator';
 import template from './register-page.tmpl';
 import '../../organisms/signup-form';
 import '../../templates/auth-template';
-import './register-page.css';
 
 const tmpl = new Templator(template);
 
