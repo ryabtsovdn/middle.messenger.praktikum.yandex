7baf2633e0a934cc21f12a1503dc9092c8ccb2de
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2021-09-17 16:04:12 +0300

    Validate signup form

--- src/components/pages/register-page/register-page.ts
+++ src/components/pages/register-page/register-page.ts
@@ -1,6 +1,5 @@
 import {Templator} from '../../../utils/templator';
 import {Block} from '../../../utils/block';
-import serializeForm from '../../../utils/serialize-form';
 import template from './register-page.tmpl';
 import '../../organisms/signup-form';
 import '../../templates/auth-template';
@@ -11,13 +10,9 @@ export class RegisterPage extends Block {
   render(): string {
     return tmpl.compile({
       form: {
-        events: {
-          submit: function (event: SubmitEvent) {
-            event.preventDefault();
-            console.log(serializeForm(event.target as HTMLFormElement));
-            window.history.pushState(null, '', '/');
-            dispatchEvent(new PopStateEvent('popstate'));
-          },
+        onSubmit: () => {
+          window.history.pushState(null, '', '/');
+          dispatchEvent(new PopStateEvent('popstate'));
         },
       },
     });
