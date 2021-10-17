693128987bbdf66c28719d639dde1be21dd66cad
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2021-10-17 10:14:00 +0300

    Fix server port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const STATIC_DIR = path.resolve(__dirname, 'dist');
 const INDEX = path.resolve(STATIC_DIR, 'index.html');
 
